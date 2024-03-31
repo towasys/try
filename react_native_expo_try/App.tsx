b90d82209e5e9d3d8b36b7f27a8ee2baa6d7f998
t_w <tw.towasys@gmail.com>
2024-03-31 21:52:45 +0900

    eas buildがmacOSかApple Developer accountが必要で撃沈した

--- react_native_expo_try/App.tsx
+++ react_native_expo_try/App.tsx
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Voice, { SpeechStartEvent } from '@react-native-voice/voice';
+import { useState } from 'react';
 
 export default function App() {
+  const [logs, set_logs] = useState<string[]>([]);
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ flex: 1, backgroundColor: "#AAA", alignItems: "center" }}>
         <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        {logs.map((log, index) => <Text key={index}>{log}</Text>)}
       </View>
       <StatusBar style="auto" />
       <View style={{ flex: 1, backgroundColor: "#0AAA", alignItems: "center" }}>
-        <Text>test on bottom</Text>
+        <Button title="Listen" onPress={() => {
+          Voice.start("ja");
+          set_logs(logs => ["test", ...logs]);
+        }} />
       </View>
     </SafeAreaView>
   );
